29c8c56e30b062aa2fb9bf4df48481858332ee7a
titanlin <titanlin@administratordeMacBook-Pro.local>
2024-06-21 10:12:41 +0800

    Create new lambda version in every deployment

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ $ npx projen build
 $ npx projen deploy
 ```
 
-In Athena query editor, you can test the following SQL. 
+In Athena query editor, you can test the following SQL. You can run UDF by different lambda versions as well.
 ```sql
 USING EXTERNAL FUNCTION unmarshal(input VARCHAR) RETURNS VARCHAR LAMBDA 'DdbUnmarshalCS'
 SELECT unmarshal('{"hello":{"S":"world"},"GG":{"S":"yy"}}')
+
+-- 
+USING EXTERNAL FUNCTION unmarshal(input VARCHAR) RETURNS VARCHAR LAMBDA 'DdbUnmarshalCS:9'
+SELECT unmarshal('{"hello":{"S":"world"},"GG":{"S":"yy"}}')
+
 ```
--- cdk/src/main/java/org/acme/MyStack.java
+++ cdk/src/main/java/org/acme/MyStack.java
@@ -8,6 +8,7 @@ import software.amazon.awscdk.Stack;
 
 import software.amazon.awscdk.BundlingOptions;
 import software.constructs.Construct;
+import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.DockerVolume;
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
@@ -19,6 +20,7 @@ import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.lambda.Version;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.s3.assets.AssetOptions;
+
 
 import static java.util.Collections.singletonList;
 import static software.amazon.awscdk.BundlingOutput.ARCHIVED;
@@ -66,9 +68,9 @@ public class MyStack extends Stack {
       .logRetention(RetentionDays.ONE_WEEK)
       .build());
 
-    Version version = Version.Builder.create(this, "version")
-      .lambda(function)
-      .build(); 
+    CfnOutput.Builder.create(this, "OutputFunctionVersion")
+      .value(function.getCurrentVersion().getFunctionArn())
+      .build();
 
   }
 }
